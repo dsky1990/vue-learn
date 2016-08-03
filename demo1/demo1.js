6faedfe4043521eab883b14102d67f2562c509b9
dsky1990 <dutian1990@gmail.com>
2016-08-04 01:05:24 +0800

    add demo1

--- demo1/demo1.js
+++ demo1/demo1.js
@@ -1,3 +1,35 @@
 /**
  * Created by dsky on 16/7/28.
  */
+
+var vm1 = new Vue({
+  el: '#example1',
+  data: {
+    message : 'hello vue!'
+  }
+});
+
+var vm2 = new Vue({
+  el: '#example2',
+  data: {
+    message: 'hello vue!'
+  }
+});
+
+var vm3 = new Vue({
+  el: '#example3',
+  data: {
+    items: [
+      {text: 'item1'},
+      {text: 'item2'},
+      {text: 'item3'}
+    ]
+  }
+});
+
+var vm4 = new Vue({
+  el: '#example4',
+  data: {
+    html: '<p class="class">hello vue!</p>'
+  }
+});
