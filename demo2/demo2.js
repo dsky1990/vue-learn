5b7979e106cc32f4bbcf20f681bc9789c6e298ee
dsky1990 <dutian1990@gmail.com>
2016-08-11 01:03:48 +0800

    add demo2

--- demo2/demo2.js
+++ demo2/demo2.js
@@ -1,3 +1,55 @@
 /**
  * Created by dsky on 16/8/11.
  */
+
+var vm1 = new Vue({
+  el: '#example1',
+  data: {
+    a: 1
+  },
+  computed: {
+    b: function () {
+      return this.a + 1;
+    }
+  }
+});
+setTimeout("vm1.a = 2",2000);
+
+var vm2 = new Vue({
+  el: '#example2',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar',
+    fullName: 'Foo Bar'
+  }
+});
+vm2.$watch('firstName', function (val) {
+  this.fullName = val + '' + this.lastName;
+});
+vm2.$watch('lastName', function (val) {
+  this.fullName = this.firstName + '' + val;
+});
+
+var vm3 = new Vue({
+  el: '#example3',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar',
+    fullName: 'Foo Bar'
+  },
+  computed: {
+    fullName: {
+      // getter
+      get: function () {
+        return this.firstName + ' ' + this.lastName;
+      },
+      // setter
+      set: function (newValue) {
+        var names = newValue.split(' ');
+        this.firstName = names[0];
+        this.lastName = names[names.length - 1];
+      }
+    }
+  }
+});
+setTimeout("vm3.fullName = 'John Doe'",2000);
